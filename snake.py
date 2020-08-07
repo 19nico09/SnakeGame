9b224ad74486aae6a133aa892a1eb867a2db4275
19nico09 <nicolas.g.astrup@gmail.com>
2020-08-07 15:53:19 +0200

    commited

--- snake.py
+++ snake.py
@@ -33,13 +33,16 @@ class Button:
         self.height = height
         self.text = text
 
-    def draw(self, screen, font):
+    def draw(self, screen, font, outline=''):
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
 
         if self.text != '':
             text = font.render(self.text, 1, BLACK)
-            screen.blit(text, (
-            int(self.x + (text - text.get_width() / 2)), int(self.y + (text - text.get_height() / 2))))
+            screen.blit(text,
+                        (int(self.x + (self.width - text.get_width() / 2)), int(self.y + (self.height - text.get_height() / 2))))
 
     def isOver(self, pos):
         if self.x < pos[0] < self.x + self.width:
@@ -47,8 +50,10 @@ class Button:
                 return True
         return False
 
+
 # Meny Buttons
-Run_button = Button(GREEN, int(WIDTH * 0.3), int(HEIGHT * 0.8), 60, 40, 'Run')
+Run_button = Button(GREEN, int(WIDTH * 0.3), int(HEIGHT * 0.8), 80, 60, 'Run')
+
 
 def start_Meny():
     print('start_Meny')
@@ -67,6 +72,14 @@ def start_Meny():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEMOTION:
+                if Run_button.isOver(pos):
+                    Run_button.color = SNAKE_RED
+                else:
+                    Run_button.color = GREEN
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Run_button.isOver(pos):
+                    run = False
 
         # draw header
         write_text('Snake', MAIN_FONT, BLACK, int(WIDTH / 2), int(HEIGHT * 0.4), win)
@@ -78,7 +91,38 @@ def start_Meny():
         pygame.display.update()
         clock.tick(15)
 
+class Snake():
+    def __init__(self, color, radius):
+        self.color = color
+        self.radius = radius
+
+    def draw(self, x, y):
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
+
+def game():
+    print('The game has began')
+    snake = Snake(BLACK, 10)
+    x = 200
+    y = 200
+    while True:
+        # set Background color
+        win.fill(WHITE)
+        snake.draw(x, y)
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        key_input = pygame.key.get_pressed()
+            if key_input[pygame.K_LEFT]:
+                x = x - 1
+            if key_input[pygame.K_RIGHT]:
+                x = x +1
+
+        pygame.display.update()
+        clock.tick(60)
 
 # game run
 start_Meny()
+game()
 pygame.quit()
